c1e13d113936abc930a85035683180d8848c57f8
Shivam <shivamdubey.dev@gmail.com>
2025-08-12 23:03:27 +0530

    Describe your update here

--- PY2/2. Indexing.py
+++ PY2/2. Indexing.py
@@ -10,3 +10,23 @@ print(ch)
 
 str2 = "Hello! I am a string 2"
 print(str2[4])
+
+str3 = "This is a string 3"
+print(str3[1:6])
+
+str4 = "I am a string 4"
+print(str4[5:len(str)])     #len(str) is used to get the length of the string
+
+str5 = "This is a string 5"
+print(str5[:7])
+
+str6 = "I am a string 6"
+print(str6[5:]) #print(str5[5:]) is used to print the string from index 5 to the end of the string
+
+
+#Negative Indexing
+
+#Negative Indexing is used to access the elements of a string by their position from the end of the string.
+
+str7 = "This is a string 7"
+print(str7[-6:-2])
