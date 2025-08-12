5a3561286e9ffc22fd169fd4093a21367575ed21
Shivam <shivamdubey.dev@gmail.com>
2025-08-12 19:36:34 +0530

    Describe your update here

--- PY2/2. Indexing.py
+++ PY2/2. Indexing.py
@@ -2,3 +2,11 @@
 #Indexing starts from 0.
 #Indexing is used to access the elements of a string by their position.
 #Indexing is used to access the elements of a string by their position.
+
+
+str = "Hello! I am a string"
+ch = str[5]
+print(ch)
+
+str2 = "Hello! I am a string 2"
+print(str2[4])
