197963289338d7afbc87bb1fd6a4545b9ca11876
Shivam <shivamdubey.dev@gmail.com>
2025-08-13 00:30:14 +0530

    Describe your update here

--- Conditionalstmt.py
+++ Conditionalstmt.py
@@ -1,1 +1,15 @@
 #Conditional statements
+
+Marks = int(input("Enter your marks: "))
+if(Marks >= 90):
+    Grade = "A"
+elif(Marks >= 80 and Marks < 90):
+    Grade = "B"
+elif(Marks >= 60 and Marks < 80):
+    Grade = "C"
+elif(Marks < 33):
+    Grade = "Fail"
+else:
+    Grade = "D"
+
+print("Your grade is:", Grade)
