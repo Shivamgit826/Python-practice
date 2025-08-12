c906e3e0d760882c19d16688ff97db8f6f4d406d
Shivam <shivamdubey.dev@gmail.com>
2025-08-12 23:24:20 +0530

    Describe your update here

---
+++ PY2/Functions.py
@@ -0,0 +1,56 @@
+#Functions
+#Functions are used to perform a specific task.
+#Functions are used to avoid code duplication.
+#Functions are used to make the code more readable.
+#Functions are used to make the code more maintainable.
+#Functions are used to make the code more reusable.
+#Functions are used to make the code more efficient.
+#Functions are used to make the code more secure.
+#Functions are used to make the code more reliable.
+#Functions are used to make the code more efficient.
+#Functions are used to make the code more secure.
+#Functions are used to make the code more reliable.
+
+
+#Syntax of a Function
+#def <function_name>():
+
+# 1. str.endswith("string") Use to check if the string ends with the given string
+str1 = "I am studying python from yt"
+print(str1.endswith("yt"))
+
+# 2. str.capitalize() Use to capitalize the first letter of the string
+str2 = "i am studying python from yt"
+print(str2.capitalize())
+
+# 3. str.replace("string1", "string2") Use to replace the given string with the new string
+str3 = "I am studying python from yt"
+print(str3.replace("python", "java"))
+
+str4 = "I am studying python from yt"
+print(str4.replace("t", "s"))
+
+# 4. str.find("string") Use to find the first occurrence of the given string
+str5 = "I am studying python from yt"
+print(str5.find("python"))
+#if the string is found, it will return the index of the first occurrence of the string
+
+str6 = "I am studying python from yt"
+print(str6.find("s"))
+
+str7 = "I am studying python from yt"
+print(str7.find("z"))
+#if the string is not found, it will return -1
+
+# 5. str.count("string") Use to count the number of occurrences of the given string
+str8 = "I am studying python from yt"
+print(str8.count("t"))
+
+str9 = "I am studying python from yt"
+print(str9.count("z"))
+#if the string is not found, it will return 0
+
+
+# 3. str.startswith("string") Use to check if the string starts with the given string
+# 5. str.index("string") Use to find the first occurrence of the given string
+# 8. str.split("string") Use to split the string into a list of strings
