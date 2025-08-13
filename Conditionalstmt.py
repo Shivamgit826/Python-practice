ce1f645563d08567c6a11c8e9a437b357c90822a
Shivam <shivamdubey.dev@gmail.com>
2025-08-13 18:33:13 +0530

    Describe your update here

--- Conditionalstmt.py
+++ Conditionalstmt.py
@@ -18,8 +18,37 @@ print("Your grade is:", Grade)
 #Nested if statements(Nesting)
 age = int(input("Enter your age: "))
 if(age >= 18):
-    if(age <= 80):
-        print("Can Drive")
+    if(age >= 80):
+        print("Can not drive")
+    else:
+        print("You can drive.")
     
 else:
     print("You cannot drive, you are underage.")
+
+
+#Practice Questions
+Num = 18
+if(Num % 2 == 0):
+    print("Even")
+else:
+    print("Odd")
+
+Num1 = int(input("Enter a number: "))
+Rem = Num1 % 2
+if(Rem == 0):
+    print("Even")
+else:
+    print("Odd")
+
+#Practice Question 2
+A = int(input("Enter first number:"))
+B = int(input("Enter second number: "))
+C = int(input("Enter third number:"))
+
+if(A > B and A > C):
+    print("A is the largest number")
+elif(B > A and B > C):
+    print("B is the greatest number")
+else:
+    print("C is the greatest number")
