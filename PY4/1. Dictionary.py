2959764a69a45700f6b4406d29d8a09c828808a7
Shivam <shivamdubey.dev@gmail.com>
2025-08-29 02:12:10 +0530

    Describe your update here

---
+++ PY4/1. Dictionary.py
@@ -0,0 +1,16 @@
+info = {
+    "key" : "value",
+    "name" : "John",
+    "age" : 30,
+    "is adult" : True,
+    "city" : "New York",
+    "learning" : ["Python", "JavaScript", "C++"],
+    "topics" : ("Math", "Science", "History")
+}
+
+print(info)
+print(type(info))
+
+print(info["name"]) # Accessing value by key
+print(info["learning"][0]) # Accessing value in a list inside the dictionary   
+ 
